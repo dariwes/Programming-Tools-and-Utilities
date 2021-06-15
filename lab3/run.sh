a6ac9138f381b081baa33317331b9beee8d83864
dariwes <shchedrovva@mail.ru>
2021-06-15 22:40:19 +0300

    Added case

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # pipenv shell
 case $1 in
+  dep)
+    cd app/Programming-Tools-and-Utilities/lab3
+    sudo sh run.sh prod
+    ;;
   prod)
-    sudo docker pull dariwes/nginx_project
-    sudo docker pull dariwes/django_project_prod
+    sudo docker pull dariwes/nginx_project:v1.0
+    sudo docker pull dariwes/django_project_prod:v1.0
     sudo docker-compose -f docker-compose.prod.yml up -d
     sudo docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input
     sudo docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations --no-input
