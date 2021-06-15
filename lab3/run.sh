becd0b5d48655dafaad26e085c22a6b139203716
dariwes <shchedrovva@mail.ru>
2021-06-15 22:02:16 +0300

    Added run.sh

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 # pipenv shell
 case $1 in
-  dev)
-    python manage.py runserver --settings=education.settings.dev
-    ;;
   prod)
     sudo docker pull dariwes/nginx_project
     sudo docker pull dariwes/django_project_prod
     sudo docker-compose -f docker-compose.prod.yml up -d
-    sudo
-    python manage.py collectstatic --noinput
-    python manage.py makemigrations
-    python manage.py migrate
-    python manage.py runserver --settings=education.settings.prod
+    sudo docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input
+    sudo docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations --no-input
+    sudo docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --no-input
     ;;
 esac
