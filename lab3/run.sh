5a8be6dd551454bfc68b10411cb5e4fc57deb30e
dariwes <shchedrovva@mail.ru>
2021-06-16 21:55:22 +0300

    Added -T flag

--- lab3/run.sh
+++ lab3/run.sh
@@ -9,8 +9,8 @@ case $1 in
     sudo docker pull dariwes/nginx_project:v1.0
     sudo docker pull dariwes/django_project_prod:v1.0
     sudo docker-compose -f docker-compose.prod.yml up -d
-    sudo docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input
-    sudo docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations --no-input
-    sudo docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --no-input
+    sudo docker-compose -f docker-compose.prod.yml exec -T web python manage.py collectstatic --no-input
+    sudo docker-compose -f docker-compose.prod.yml exec -T web python manage.py makemigrations --no-input
+    sudo docker-compose -f docker-compose.prod.yml exec -T web python manage.py migrate --no-input
     ;;
 esac
